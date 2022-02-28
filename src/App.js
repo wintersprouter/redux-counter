6d90dd99c3e937f9d989bb3903d94a28dcb26a5f
chiahui <ashleyhsueh33@gmail.com>
2022-02-28 22:58:42 +0800

    feat: reading and dispatching from a new slice

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import Counter from './components/Counter';
-
+import { Fragment } from 'react'
+import { useSelector } from 'react-redux'
+import Counter from './components/Counter'
+import Header from './components/Header'
+import Auth from './components/Auth'
+import UserProfile from './components/UserProfile'
 
 function App() {
+  const auth = useSelector(state => state.auth.isAuthenticated)
   return (
-    <Counter />
-  );
+    <Fragment>
+      <Header></Header>
+      {!auth && <Auth />}
+      {auth && <UserProfile />}
+      <Counter />
+    </Fragment>
+  )
 }
 
-export default App;
+export default App
